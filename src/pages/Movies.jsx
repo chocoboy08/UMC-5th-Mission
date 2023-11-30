7d0b3cb46f14a78bcc9606f5c35f50d06cf13f79
chocoboy08 <chocoboy0508@gmail.com>
2023-11-30 17:05:14 +0900

    add tv page

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
-import Movie from "../movie/Movie";
-import { movies } from "../movie/movieList";
+import Content from "./components/Content";
+import { movies } from "../assets/movieList";
 
 const Background = styled.div({
   width: "100%",
 });
-const MovieContainer = styled.div({
+const ContentsContainer = styled.div({
   display: "grid",
   gridTemplateRows: "repeat(2,1fr)",
   gridTemplateColumns: " repeat(8,1fr)",
@@ -18,10 +18,10 @@ function Movies() {
   const MOVIES = movies.results;
   return (
     <Background>
-      <MovieContainer>
+      <ContentsContainer>
         {MOVIES.map((item) => {
           return (
-            <Movie
+            <Content
               title={item.title}
               img={item.poster_path}
               rate={item.vote_average}
@@ -30,7 +30,7 @@ function Movies() {
             />
           );
         })}
-      </MovieContainer>
+      </ContentsContainer>
     </Background>
   );
 }
--- src/pages/TV.jsx
+++ src/pages/TV.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
-
+import { styled } from 'styled-components';
+import { Programs } from '../assets/tvDummy.js';
+import Content from './components/Content'
+const Background = styled.div({
+  width: "100%",
+});
+const ContentsContainer = styled.div({
+  display: "grid",
+  gridTemplateRows: "repeat(2,1fr)",
+  gridTemplateColumns: " repeat(8,1fr)",
+  justifyContent: "center",
+  alignItems: "center",
+});
 function TV() {
+  const PROGRAMS = Programs.results;
   return (
-    <div>TV</div>
+    <Background>
+      <ContentsContainer>
+        {PROGRAMS.map((item) => {
+          return (
+            <Content
+              title={item.name}
+              img={item.poster_path}
+              rate={item.vote_average}
+              detail={item.overview}
+              key={`Programs-item-${item.title}`}
+            />
+          );
+        })}
+      </ContentsContainer>
+    </Background>
   )
 }
 
